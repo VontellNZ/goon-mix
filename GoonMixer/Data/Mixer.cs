51968754512b7ccc87b1268d9309deb7631bce35
Brendan Childs <vontellnz@gmail.com>
2023-05-04 17:24:44 +1200

    Add a file input element to the main page

--- GoonMixer/Data/Mixer.cs
+++ GoonMixer/Data/Mixer.cs
@@ -1,20 +1,26 @@
+using GoonMixer.Data.Enums;
 using GoonMixer.Data.Interfaces;
 
 namespace GoonMixer.Data
 {
-    public class Mixer
+    public static class Mixer
     {
-        private IMixingTechnique MixingTechnique { get; set; }
-        private List<Song> Songs { get; set; }
-
-        public Mixer(IMixingTechnique mixingTechnique, List<Song> songs)
+        public static async Task Start(IMixingTechnique mixingTechnique)
         {
-            MixingTechnique = mixingTechnique;
-            Songs = songs;
-        }
+            await Task.Delay(1);
 
-        public void Start()
-        {
+            var mainSong = new Song("Solar System", "Sub Focus", "Solar System / Siren", "Drum & Bass", 
+                174, new TimeSpan(0, 4, 48), new Key("F", Scale.Minor, Signature.None, new CamelotScale(4, "A")));
+            var harmonicMatch = new Song("Devotion (feat. Cameron Hayes)", "Dimension", "Organ", "Drum & Bass", 
+                174, new TimeSpan(0, 3, 10), new Key("F", Scale.Minor, Signature.None, new CamelotScale(4, "A")));
+            var nonMatch = new Song("Afterglow", "Wilkinson", " Lazers Not Included", "Drum & Bass", 
+                174, new TimeSpan(0, 3, 45), new Key("G", Scale.Major, Signature.None, new CamelotScale(9, "B")));
+            var songs = new List<Song>{ mainSong, harmonicMatch, nonMatch };   
+
+            var result = mixingTechnique.GetSuggestedSongs(mainSong, songs);
+
+            foreach (var song in result)
+                Console.WriteLine(song.Title);
         }
     }
 }
